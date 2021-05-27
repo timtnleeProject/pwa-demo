fca25a26799de87d31d14b6706c2b441810c4dd1
timtnleeProject <litingen1995@gmail.com>
2021-05-27 15:32:41 +0800

    feat: 🎸 Basic functionality

---
+++ src/components/Item.js
@@ -0,0 +1,48 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { css } from "@emotion/react";
+
+export function Wrap(props) {
+  return (
+    <div
+      css={css`
+        display: flex;
+        flex-wrap: wrap;
+      `}
+      {...props}
+    ></div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <div
+      css={css`
+        flex: 1 1 50%;
+        padding: 8px;
+        border: 1px solid lightblue;
+        .img {
+          background-image: url(${item.image});
+          background-size: cover;
+          background-repeat: no-repeat;
+          background-position: center;
+          width: 100%;
+          padding-top: 50%;
+        }
+        .title {
+          font-weight: bold;
+          font-size: 1.5rem;
+        }
+      `}
+    >
+      <div className="img"></div>
+      <div className="title">{item.title}</div>
+    </div>
+  );
+}
+
+Item.propTypes = {
+  item: PropTypes.object,
+};
+
+export default Item;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,79 @@
-import { useEffect } from "react";
+import { css } from "@emotion/react";
+import { useEffect, useState } from "react";
 import { render } from "react-dom";
+import Item, { Wrap } from "./components/Item";
 import "./index.scss";
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [lists, setLists] = useState([]);
+  const [length, setLength] = useState(0);
+  const [page, setPage] = useState(1);
   useEffect(() => {
-    fetch("/api/list?page=1")
+    setLoading(true);
+    fetch(`/api/list?page=${page}`)
       .then((res) => res.json())
       .then((res) => {
-        console.log(res);
+        setLists(res.data);
+        setLength(res.length);
+        setLoading(false);
       });
-  }, []);
+  }, [page]);
   return (
     <div>
-      <h1>PWA DEMO</h1>
+      <header>
+        <h1>PWA DEMO</h1>
+      </header>
+      <main
+        css={css`
+          position: relative;
+        `}
+      >
+        {Array.from(Array(length))
+          .map((_, i) => i + 1)
+          .map((i) => (
+            <button
+              key={i}
+              onClick={() => setPage(i)}
+              css={css`
+                padding: 0 12px;
+                border: 1px solid blue;
+                cursor: pointer;
+                color: blue;
+                background-color: white;
+                ${page === i &&
+                css`
+                  color: white;
+                  background-color: blue;
+                `}
+              `}
+            >
+              {i}
+            </button>
+          ))}
+        <Wrap>
+          {lists.map((item, i) => (
+            <Item key={i} item={item} />
+          ))}
+        </Wrap>
+        {loading && (
+          <div
+            css={css`
+              position: absolute;
+              top: 0;
+              left: 0;
+              width: 100%;
+              height: 100%;
+              display: flex;
+              justify-content: center;
+              padding-top: 5vh;
+              background-color: rgba(255, 255, 255, 0.5);
+            `}
+          >
+            LOADING...
+          </div>
+        )}
+      </main>
     </div>
   );
 };
