92f5df662f10d6bebe8e4ab689d97218c7dcc5b9
ext-timothyl <timothy_lee@trendmicro.com>
2021-06-08 15:37:13 +0800

    feat: 🎸 clear old cache mechanism

--- public/sw.js
+++ public/sw.js
@@ -1,16 +1,20 @@
-const cacheName = "pwa-demo-cache";
+const version = "v1";
+const CACHE_NAME = "pwa-demo-cache-" + version;
 
 self.addEventListener("install", function (event) {
   console.log("[INSTALL SW]");
   event.waitUntil(
-    caches.open(cacheName).then(function (cache) {
+    caches.open(CACHE_NAME).then(function (cache) {
       return fetch("/assets-manifest.json")
         .then(function (response) {
           return response.json();
         })
         .then(function (assets) {
-          console.log("add caches assets");
-          return cache.addAll(Object.values(assets));
+          const list = Object.values(assets).filter(
+            (path) => !path.match(/(images\/)|(icons\/)|(screenshots\/)/)
+          );
+          console.log("[ADD CACHES]: ", list);
+          return cache.addAll(list);
         })
         .catch((e) => {
           console.log("cannot get assets manifest", e);
@@ -20,19 +24,48 @@ self.addEventListener("install", function (event) {
 });
 
 self.addEventListener("fetch", function (event) {
-  console.log(event.request.url);
+  console.log(`[FETCH EVENT FIRES: ${event.request.url}]`);
   event.respondWith(
     caches.match(event.request).then(function (response) {
-      return (
-        response ||
-        fetch(event.request)
-          .then(function (response) {
-            return caches.open(cacheName).then(function (cache) {
-              cache.put(event.request, response.clone());
-              return response;
-            });
-          })
-          .catch(function () {})
+      if (response) {
+        console.log(`[RETURN FROM CACHE ${event.request.url}]`);
+        return response;
+      }
+      console.log(`[FETCH ${event.request.url}]`);
+      return fetch(event.request)
+        .then(function (response) {
+          if (
+            !response ||
+            response.status !== 200 ||
+            response.type !== "basic"
+          ) {
+            return response;
+          }
+          const cloneResponse = response.clone();
+          caches.open(CACHE_NAME).then(function (cache) {
+            cache.put(event.request, cloneResponse);
+          });
+          return response;
+        })
+        .catch(function () {});
+    })
+  );
+});
+
+self.addEventListener("activate", function (event) {
+  console.log("[ACTIVATE SW]");
+  const cacheAllowlist = [CACHE_NAME];
+
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          if (cacheAllowlist.indexOf(cacheName) === -1) {
+            console.log(`[CLEAR CACHE ${cacheName}]`);
+            return caches.delete(cacheName);
+          }
+          return Promise.resolve();
+        })
       );
     })
   );
