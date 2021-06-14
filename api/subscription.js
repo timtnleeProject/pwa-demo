68a444edd16ddb8d3e53fc6381a3b9c705ee3d24
timtnleeProject <litingen1995@gmail.com>
2021-06-14 14:41:36 +0800

    feat: 🎸 Use subs lists

--- api/subscription.js
+++ api/subscription.js
@@ -1,5 +1,7 @@
 import webpush from "web-push";
 
+const subs = [];
+
 const vapidKeys = {
   publicKey:
     "BBLWcX2o9qUk1S5cS_58QK-B7hjw6yf-rrLrqRpi_z9S0OgaoUD9Y2bZI9ZThMfcu6YBSXkNVPKH2Mx2QThnUzQ",
@@ -12,16 +14,17 @@ webpush.setVapidDetails(
   vapidKeys.privateKey
 );
 
-const pushMessageLater = (subscription) =>
-  new Promise((resolve, reject) => {
-    setTimeout(() => {
-      webpush.sendNotification(subscription, "Message from server!");
-      resolve();
-    }, 10000);
-  });
+const pushMessageLater = (subscription) => {
+  if (!subs.find((s) => s === subscription)) {
+    subs.push(subscription);
+  }
+  return Promise.allSettled(
+    subs.map((s) => webpush.sendNotification(s, "Message from server!"))
+  );
+};
 
 export default async (req, res) => {
   const subscription = req.body;
   await pushMessageLater(subscription);
-  res.json(vapidKeys);
+  res.json(subs);
 };
